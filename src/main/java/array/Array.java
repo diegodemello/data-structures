58d68e3ac8234cfc337ddc1951960e20d5848476
Diego de Mello <diegodemello027@gmail.com>
2025-05-15 22:23:42 -0300

    Métodos adicionar elemento, ver quantidade e imprimir elementos do Array.

--- src/main/java/array/Array.java
+++ src/main/java/array/Array.java
@@ -1,9 +1,43 @@
 package array;
 
+import java.util.Arrays;
+
 public class Array {
     private String[] elements;
+    private int sizeArray;
 
     public Array(int size) {
         this.elements = new String[size];
+        this.sizeArray = 0;
+    }
+
+    public boolean addElement(String element){
+        if (sizeArray < elements.length) {
+            this.elements[this.sizeArray] = element;
+            this.sizeArray++;
+            return true;
+        }
+        return false;
+    }
+
+    public int getSize(){
+        return this.sizeArray;
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder s = new StringBuilder();
+
+        s.append("[");
+        for(int i = 0; i < this.sizeArray-1; i++){
+            s.append(this.elements[i]);
+            s.append(", ");
+        }
+        if(this.sizeArray>0){
+            s.append(this.elements[this.sizeArray-1]);
+        }
+        s.append("]");
+
+        return s.toString();
     }
 }
--- src/main/java/array/test/Main.java
+++ src/main/java/array/test/Main.java
@@ -5,5 +5,9 @@ import array.Array;
 public class Main {
     public static void main(String[] args) {
         Array array = new Array(5);
+        array.addElement("Diego");
+
+        System.out.println(array.getSize());;
+        System.out.println(array);
     }
 }
